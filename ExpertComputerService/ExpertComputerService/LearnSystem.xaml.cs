d19f5a7a6eb03451d2c301feb4c48be77940a87c
Kirill <kiiirrrk@gmail.com>
2015-12-10 03:05:59 +0300

    KirKinator BETA Version 0.2.5. very many changes

--- ExpertComputerService/ExpertComputerService/LearnSystem.xaml.cs
+++ ExpertComputerService/ExpertComputerService/LearnSystem.xaml.cs
@@ -16,6 +16,10 @@ using Domain;
 using System.Data;
 using Configurate;
 using ExpertComputerService.LearnSystemsMaterials;
+using System.IO;
+using Microsoft.Win32;
+using ExpertCore;
+using System.Windows.Media.Animation;
 
 namespace ExpertComputerService
 {
@@ -50,36 +54,63 @@ namespace ExpertComputerService
         selectedButtons SelectBut;
 
        
+        private void DataGridUpdate()
+        {
+            DoubleAnimation db = new DoubleAnimation();
+            db.From = 0;
+            db.To = 1.0;
+            db.Duration = new Duration(TimeSpan.FromSeconds(0.4));
+            db.AutoReverse = false;
+            db.RepeatBehavior = RepeatBehavior.Forever;
 
+            db.RepeatBehavior = new RepeatBehavior(1);
+            dgrid.BeginAnimation(OpacityProperty, db);
+            
+            switch (SelectBut)
+            {
+                case selectedButtons.HeroesBut:
+                    List<PHero> Hp = new List<PHero>();
+                    foreach (var item in new Repository().GetHeroesSource().ToList())
+                    {
+                        Hp.Add(new PHero { NameHeroes = item.NameHeroes, WeigthHero = item.WeigthHero, TextHero = item.TextHero });
+                    }
+
+                    dgrid.ItemsSource = Hp.ToList();
+                    break;
+                case selectedButtons.QuestionsBut:
+                    var a = (new Repository().GetQuestionsSource()).Select(d => new { d.NameQestion }).Distinct().ToList();
+                    dgrid.ItemsSource = a.ToList();
+                    break;
+                case selectedButtons.DominatingBut:
+                    dgrid.ItemsSource = (new Repository().GetQuestionsSource()).Select(d => new
+                    {
+                        d.NameQestion,
+                        d.NameHeroes,
+                        d.OtvetQuest1,
+                        d.OtvetQuest2,
+                        d.OtvetQuest3,
+                        d.OtvetQuest4,
+                        d.OtvetQuest5,
+                        d.OtvetSelected
+                    }).ToList();
+                    break;
+            }
+        }
 
         private void buttonHeroes_Click(object sender, RoutedEventArgs e)
         {
-            var a = (new Repository().GetHeroesSource()).Select(d => new { d.NameHeroes, d.TextHero, d.WeigthHero }).ToList();
-      //      dgrid.ItemsSource = (new Repository().GetHeroesSource()).Select(d => new { d.NameHeroes, d.TextHero, d.WeigthHero }).ToList();
-            dgrid.ItemsSource = a.ToList();
-            SelectBut = selectedButtons.HeroesBut; 
-            
+            SelectBut = selectedButtons.HeroesBut;
+            DataGridUpdate();
         }
         private void buttonQuestions_Click(object sender, RoutedEventArgs e)
         {
-            var a = (new Repository().GetQuestionsSource()).Select(d => new { d.NameQestion }).Distinct().ToList();
-            dgrid.ItemsSource = a.ToList();
             SelectBut = selectedButtons.QuestionsBut;
+            DataGridUpdate();
         }
         private void buttonDominations_Click(object sender, RoutedEventArgs e)
         {
-            dgrid.ItemsSource = (new Repository().GetQuestionsSource()).Select(d => new
-            {
-                d.NameQestion,
-                d.NameHeroes,
-                d.OtvetQuest1,
-                d.OtvetQuest2,
-                d.OtvetQuest3,
-                d.OtvetQuest4,
-                d.OtvetQuest5,
-                d.OtvetSelected
-            }).ToList();
             SelectBut = selectedButtons.DominatingBut;
+            DataGridUpdate();
         }
         
 
@@ -102,7 +133,7 @@ namespace ExpertComputerService
                 {
                     case selectedButtons.QuestionsBut:
                         
-                        ERep = new Repository().RemoveQuestion(textBoxAddQuestion.Text);
+          //              ERep = new Repository().RemoveQuestion(textBoxAddQuestion.Text);
                         break;
                                    
                }
@@ -128,11 +159,179 @@ namespace ExpertComputerService
             this.Close();
         }
 
-        private void buttonAddQuest_Click(object sender, RoutedEventArgs e)
+        private void button_upd_Click(object sender, RoutedEventArgs e)
         {
-            if(textBoxAddQuestion.Text!="")
-                try {  new Repository().AddQuestion(textBoxAddQuestion.Text); ProgressSuccessLabel.Visibility = Visibility.Visible;  }
-                catch(Exception ex) { MessageBox.Show(ex.Message); }
+            
         }
+
+        private void dgrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SelectBut == selectedButtons.HeroesBut)
+            {
+                PHero hero = dgrid.SelectedItem as PHero;
+                if (hero != null)
+                {
+                    try
+                    {
+                        LabelUpd.Content = hero.NameHeroes;
+                        string OldHero2 = hero.NameHeroes.Substring(hero.NameHeroes.IndexOf("(") + 1);
+                        OldHero2 = OldHero2.Substring(0, OldHero2.Length - 1);
+                        string OldHero1 = hero.NameHeroes.Substring(0, hero.NameHeroes.Length - OldHero2.Length - 2);
+
+
+                        OldName1.Text = OldHero1;
+                        OldName2.Text = OldHero2;
+                        NewName1.Text = OldHero1;
+                        NewName2.Text = OldHero2;
+                    }
+                    catch
+                    {
+                        //           MessageBox.Show("Названия героя заданы некорректо, исправьте на корректное название");
+                        OldName1.Text = "Некорректно";
+                        OldName2.Text = "Некорректно";
+                        NewName1.Text = "";
+                        NewName2.Text = "";
+                    }
+                    string imgpatch = ExpConfig.Default.patchImages + hero.NameHeroes;
+
+                    if (File.Exists(imgpatch + ".jpg"))
+                        initializeImage(imgpatch, ".jpg");
+                    else if (File.Exists(imgpatch + ".png"))
+                        initializeImage(imgpatch, ".png");
+                    else if (File.Exists(imgpatch + ".gif"))
+                        initializeImage(imgpatch, ".gif");
+                    else
+                        image.Source = new BitmapImage(new Uri("pack://application:,,,/Media/Unknown_Flag.png"));
+                }
+            }
+        }
+        #region Operations with Heroes
+        string patchImage = null; //путь к картинке
+
+
+        #region Operations with image
+        private void initializeImage(string imgpatch, string ImgFormat)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                FileStream stream = new FileStream(imgpatch + ImgFormat, FileMode.Open, FileAccess.Read);
+                ms.SetLength(stream.Length);
+                stream.Read(ms.GetBuffer(), 0, (int)stream.Length);
+
+                ms.Flush();
+                stream.Close();
+
+                BitmapImage src = new BitmapImage();
+                src.BeginInit();
+                src.StreamSource = ms;
+                src.EndInit();
+                image.Source = src;
+                patchImage = imgpatch + ImgFormat;
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+        }
+
+        private void loadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Файлы изображений|*.png;*.jpg;*.gif|Все файлы(*.*)|*.*";
+            openFileDialog1.Title = "Выберите картинку героя";
+            try
+            {
+                if (openFileDialog1.ShowDialog() == true)
+                {
+                    patchImage = openFileDialog1.FileName;
+                    image.Source = new BitmapImage(new Uri(openFileDialog1.FileName));
+                }
+            }
+            catch
+            { patchImage = null; }
+        }   //загрузка картинки в имедж
+        private bool ImageSave(string HeroName)
+        {
+            bool errors = false;
+            if (patchImage != null)
+            {
+                try
+                {
+                    if (!Directory.Exists(ExpConfig.Default.patchImages))
+                    {
+                        Directory.CreateDirectory(ExpConfig.Default.patchImages);
+                    }
+                    System.IO.File.Copy(patchImage, ExpConfig.Default.patchImages + HeroName + patchImage.Substring(patchImage.Length - 4, 4), true);
+                }
+                catch (Exception ex)
+                {
+                    var result = MessageBox.Show("Не удалось сохранить картинку. Продолжить отправку?: \n" + ex.Message, "Ошибка сохранения фото", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    if (result == MessageBoxResult.No)
+                        errors = true;
+                }
+            }
+            return errors;
+        }   //сохранение картинки в папке с соответствующим имененм
+        #endregion
+
+        private void buttonImageBackground_Click(object sender, RoutedEventArgs e)
+        {
+            buttonImageBackground.Visibility = Visibility.Collapsed;
+        }
+
+        private void buttonImageChange_Click(object sender, RoutedEventArgs e)
+        {
+            buttonImageBackground.Visibility = Visibility.Visible;
+            var brush = new ImageBrush();
+            brush.ImageSource = image.Source;
+            buttonImageBackground.Background = brush;
+        }
+
+        private void buttonSwithImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Файлы изображений|*.png;*.jpg;*.gif|Все файлы(*.*)|*.*";
+            openFileDialog1.Title = "Выберите картинку героя";
+            try
+            {
+                if (openFileDialog1.ShowDialog() == true)
+                {
+                    patchImage = openFileDialog1.FileName;
+                    image.Source = new BitmapImage(new Uri(openFileDialog1.FileName));
+                }
+            }
+            catch
+            { patchImage = null; }
+        }
+
+        private void buttonSaveHeroes_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewName1.Text != "" && NewName2.Text != "")
+            {
+                if (NewName1.Text == OldName1.Text && NewName2.Text == OldName2.Text)
+                {
+                    ImageSave(Convert.ToString(LabelUpd.Content));
+                    return;
+                }
+                //
+
+                string NewHeroName = NewName1.Text + "(" + NewName2.Text + ")";
+                string OldHeroName = Convert.ToString(LabelUpd.Content);
+
+                if (ImageSave(NewHeroName))
+                    return;
+
+                Exception ex = new Repository().updHero(OldHeroName, NewHeroName);
+                if (ex != null)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                new Repository().updHero(OldHeroName, NewHeroName);
+                DataGridUpdate();
+                ProgressSuccessLabel.Visibility = Visibility.Visible;
+              //  MessageBox.Show("Данные сохранены!");
+            }
+        }
+        #endregion
     }
 }
